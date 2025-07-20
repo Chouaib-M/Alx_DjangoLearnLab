df24940921013388c7fe48b6e246e04a7629dc75
CnssStockManag <stockmanagcnss@gmail.com>
2025-07-20 19:06:15 +0100

    Add FBV list_books and CBV LibraryDetailView with templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
