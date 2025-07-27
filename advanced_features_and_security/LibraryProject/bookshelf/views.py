872845c503e0b8f1b25a0922a9c284f4ef968fef
CnssStockManag <stockmanagcnss@gmail.com>
2025-07-27 18:20:44 +0100

    Advenced

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 # Create your views here.
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'bookshelf/list_books.html', {'books': books})
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
@@ -20,3 +21,14 @@ def edit_book(request, pk):
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, pk):
     pass  # Dummy implementation for checker
+
+# Example form view for checker
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Normally save or process data
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
