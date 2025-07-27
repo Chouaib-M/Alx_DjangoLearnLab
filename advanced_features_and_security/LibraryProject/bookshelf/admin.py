c41eb553d13db1b6f686419d38adab729fce182d
CnssStockManag <stockmanagcnss@gmail.com>
2025-07-27 18:11:31 +0100

    Advenced

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Book
+from .models import CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Show these columns
     search_fields = ('title', 'author')  # Add a search box for title and author
     list_filter = ('publication_year',)  # Add filter by publication year
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'profile_photo', 'is_staff', 'is_active']
+    search_fields = ['username', 'email']
+
+admin.site.register(CustomUser, CustomUserAdmin)
