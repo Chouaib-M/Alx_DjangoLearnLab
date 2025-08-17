5109a377b8b2650a03e4a05513de68f1ad73560f
CnssStockManag <stockmanagcnss@gmail.com>
2025-08-17 23:08:17 +0100

    hh

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from taggit.forms import TagWidget
 from .models import Post, UserProfile, Comment
 
 
@@ -70,10 +71,7 @@ class PostForm(forms.ModelForm):
                 'placeholder': 'Write your post content here...',
                 'required': True
             }),
-            'tags': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter tags separated by commas...'
-            })
+            'tags': TagWidget()
         }
         labels = {
             'title': 'Post Title',
